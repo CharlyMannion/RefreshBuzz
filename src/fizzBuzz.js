3c6782d849145317562cbade825fea7c441cff9f
Charlotte Mannion <charlottemannion100@gmail.com>
2018-09-26 18:58:13 +0100

    completely refactors play function to use divisor function

--- src/fizzBuzz.js
+++ src/fizzBuzz.js
@@ -4,9 +4,9 @@ function FizzBuzz() {
 FizzBuzz.prototype.play = function(number) {
     if (this._isDivisibleBy(15, number)) {
         return 'FizzBuzz'
-    } else if (number % 3 === 0) {
+    } else if (this._isDivisibleBy(3, number)) {
         return 'Fizz';
-    } else if (number % 5 === 0) {
+    } else if (this._isDivisibleBy(5, number)) {
         return 'Buzz';
     } else {
         return number;
